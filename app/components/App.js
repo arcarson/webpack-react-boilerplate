c3838d30efb916fb71d3568ec202062dec452785
Andy Carson <andrew.carson@silverpond.com.au>
2016-08-08 15:00:37 +1000

    include normalize

--- app/components/App.js
+++ app/components/App.js
@@ -2,6 +2,8 @@ import React from "react"
 import { StyleRoot } from "radium"
 import HelloWorld from "components/HelloWorld"
 
+import "normalize.css"
+
 export default class App extends React.Component {
   render() {
     return (
