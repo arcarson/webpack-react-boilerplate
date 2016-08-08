25feb91ce16810c94cb3f936449bbccae46151c3
Andy Carson <andrew.carson@silverpond.com.au>
2016-08-08 15:50:11 +1000

    define routes as data structure

--- app/components/Router.js
+++ app/components/Router.js
@@ -1,13 +1,25 @@
 import React from "react"
 import { Router, Route, Link, browserHistory } from "react-router"
+
 import App from "components/App"
+import HelloWorld from "components/HelloWorld"
 
 export default class AppRouter extends React.Component {
   render() {
+
+    const routes = {
+      path: "/",
+      component: App,
+      childRoutes: [
+    	{
+    	  path: "hello-world",
+    	  component: HelloWorld
+    	}
+      ]
+    }
+
     return (
-      <Router history={browserHistory}>
-        <Route path="/" component={App}></Route>
-      </Router>
+      <Router history={browserHistory} routes={routes}></Router>
     )
   }
 }
