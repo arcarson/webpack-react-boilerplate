1c84ba6170a85f3a6b251781b0917be25e4a272d
Andy Carson <andrew.carson@silverpond.com.au>
2016-08-21 21:05:21 +1000

    insert html defaults into style tag in head

--- app/settings.js
+++ app/settings.js
@@ -1,8 +1,20 @@
+import styleInject from 'style-inject'
+import toCss from 'to-css'
+import { kebabCase, forIn } from 'lodash'
 import typescale from 'typescale'
 import paletteGenerator from 'palette-generator'
-
-export const type = typescale()
 
 export const palette = paletteGenerator(215, '74%', '58%', { shadeVariation: '20%',
                                                              hueIncrement: 15,
                                                              scheme: 'accentedAnalogous' })
+
+
+export const type = typescale({ fontFamily: 'Heebo' })
+
+const css = { html: {} }
+
+forIn(type.html, (value, key) => {
+  css.html[kebabCase(key)] = value
+})
+
+styleInject(toCss(css))
